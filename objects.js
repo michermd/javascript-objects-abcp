63162de016ecca261f386478b2ae084fad1e070b
Daniel Micher <michermd@gmail.com>
2018-01-12 02:41:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  return Object.assign({}, delete playlist.artistName)
+  return(delete playlist.artistName);
 }
 // function updateObjectWithObject(targetObject, updatesObject) {
 //   return Object.assign({}, targetObject, updatesObject)
